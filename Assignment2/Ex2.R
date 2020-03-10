10d8ec45322d0c50479f87ef7a52fc63e3525b37
vebjorre <vebjorre@stud.ntnu.no>
2020-03-10 18:00:34 +0100

    figures

--- Assignment2/Ex2.R
+++ Assignment2/Ex2.R
@@ -6,26 +6,28 @@ library(gridExtra)
 # Load data
 data <- as_tibble(read.table("https://www.math.ntnu.no/emner/TMA4250/2020v/Exercise2/obsprob.txt", header=TRUE))
 data["pines"] <- read.table("https://www.math.ntnu.no/emner/TMA4250/2020v/Exercise2/obspines.txt", header=TRUE)[3]
-tot_area <- 300^2
-node_area <- 100
-n <- nrow(data)
 
 # Plot number of observed pines and observation probabilities
 p <- ggplot(data, aes(x, y))
 fill <- scale_fill_gradient(low="burlywood", high="darkgreen")
 theme <- theme_minimal()
 p + geom_raster(aes(fill=pines)) + fill + theme
-fill <- scale_fill_gradient(low="darkgreen", high="burlywood")
+fill <- scale_fill_gradient(low="black", high="lightgrey")
 p + geom_raster(aes(fill=alpha)) + fill + theme
 
+#Estimate lambda
+node_area <- 100
+n <- nrow(data)
 pihat <- n/sum(data$alpha)
 lambdahat <- pihat / node_area
 
+#Simulate count model from prior distribution
 nsims <- 6
 prior.sims <- replicate(nsims, rpois(n,pihat))
+
+#Make plots of count models (Not included in report)
 prior.plots <- vector("list", nsims)
 fill <- scale_fill_gradient(low="burlywood", high="darkgreen")
-
 for (i in 1:nsims){
   d <- data
   d["pines"] <- prior.sims[,i]
@@ -34,6 +36,7 @@ for (i in 1:nsims){
 }
 prior.plots
 
+#Function for simulating location model given count model
 counts2loc <- function(sim){
   x.sim <- matrix(NA,nrow=sum(sim),ncol=2)
   nk <- 0
@@ -48,16 +51,22 @@ counts2loc <- function(sim){
   }
   return (x.sim)
 }
+
+#Simulate prior and plot location models
 prior.loc <- vector("list", nsims)
 for (i in 1:nsims){
   prior.loc[[i]] <- counts2loc(prior.sims[,i])
-  plot(prior.loc[[i]], col="darkgreen")
+  plot(prior.loc[[i]], col="darkgreen", xlab="x", ylab="y")
 }
 
+#Posterior intensity
 intensity.post <- pihat*(1 - data$alpha)
-post.counts <- replicate(nsims, rpois(n, intensity.post))
-post.plots <- vector("list",nsims)
 
+#Simulate posterior count models
+post.counts <- replicate(nsims, rpois(n, intensity.post))
+
+#Plot posterior count models
+post.plots <- vector("list",nsims)
 for (i in 1:nsims){
   d <- data
   d["pines"] <- post.counts[,i]
@@ -66,10 +75,11 @@ for (i in 1:nsims){
 }
 post.plots
 
+#Simulate and plot posterior location models
 post.loc <- vector("list", nsims)
 for (i in 1:nsims){
   post.loc[[i]] <- counts2loc(post.counts[,i])
-  plot(post.loc[[i]], col="darkgreen")
+  plot(post.loc[[i]], col="darkgreen", xlab="x", ylab="y")
 }
 
 nsims <- 100
